e1771edc8b697150b34f6efa88295d3a56f9d4fb
Rudolf Polzer <divverent@xonotic.org>
2011-07-12 20:16:53 +0200

    make unneeded symbols invisible

--- s2tc_compressor.h
+++ s2tc_compressor.h
@@ -1,15 +1,21 @@
 #ifndef S2TC_COMPRESSOR_H
 #define S2TC_COMPRESSOR_H
 
+// note: this is a C header file!
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void rgb565_image(unsigned char *out, const unsigned char *rgba, int w, int h, int alpharange);
 
-enum DxtMode
+typedef enum
 {
 	DXT1,
 	DXT3,
 	DXT5
-};
-enum ColorDistMode
+} DxtMode;
+typedef enum
 {
 	RGB,
 	YUV,
@@ -18,8 +24,12 @@ enum ColorDistMode
 	LAB,
 	AVG,
 	NORMALMAP
-};
+} ColorDistMode;
 
 void s2tc_encode_block(unsigned char *out, const unsigned char *rgba, int iw, int w, int h, DxtMode dxt, ColorDistMode cd, int nrandom);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
