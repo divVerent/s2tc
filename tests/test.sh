8b0c3fa7b77a7d8b8cb24c0de8185216b6d80d15
Rudolf Polzer <divverent@xonotic.org>
2011-07-15 12:10:44 +0200

    loop the refinement in the test

--- tests/test.sh
+++ tests/test.sh
@@ -241,9 +241,9 @@ for i in dxtfail floor_tread01 floor_tread01_norm_dxt5 floor_tread01_norm_dxt3 f
 		html "$i"-nvcompress.dds
 	fi
 
-	S2TC_COLORDIST_MODE=$goodmetric S2TC_RANDOM_COLORS=32 S2TC_REFINE_COLORS=CHECK  t "$i".tga "$i"-rand32-mrgb-r.dds bin/s2tc -t $fourcc
-	S2TC_COLORDIST_MODE=WAVG        S2TC_RANDOM_COLORS=32 S2TC_REFINE_COLORS=CHECK  t "$i".tga "$i"-rand32-wavg-r.dds bin/s2tc -t $fourcc
-	S2TC_COLORDIST_MODE=AVG         S2TC_RANDOM_COLORS=32 S2TC_REFINE_COLORS=CHECK  t "$i".tga "$i"-rand32-avg-r.dds  bin/s2tc -t $fourcc
+	S2TC_COLORDIST_MODE=$goodmetric S2TC_RANDOM_COLORS=32 S2TC_REFINE_COLORS=LOOP   t "$i".tga "$i"-rand32-mrgb-r.dds bin/s2tc -t $fourcc
+	S2TC_COLORDIST_MODE=WAVG        S2TC_RANDOM_COLORS=32 S2TC_REFINE_COLORS=LOOP   t "$i".tga "$i"-rand32-wavg-r.dds bin/s2tc -t $fourcc
+	S2TC_COLORDIST_MODE=AVG         S2TC_RANDOM_COLORS=32 S2TC_REFINE_COLORS=LOOP   t "$i".tga "$i"-rand32-avg-r.dds  bin/s2tc -t $fourcc
 	if $use_libtxc_dxtn; then
 		LD_PRELOAD=/usr/lib/libtxc_dxtn.so                                      t "$i".tga "$i"-libtxc_dxtn.dds   bin/s2tc -t $fourcc
 		unset LD_PRELOAD
