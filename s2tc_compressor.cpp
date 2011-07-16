4a4083ba3ea89ab29a156b90e0304ae5a3d9a95a
Rudolf Polzer <divverent@xonotic.org>
2011-07-16 13:55:02 +0200

    fix an inverse logic bug

--- s2tc_compressor.cpp
+++ s2tc_compressor.cpp
@@ -730,7 +730,7 @@ namespace
 							c[4].r = rgba[(x + y * iw) * 4 + 2];
 							c[4].g = rgba[(x + y * iw) * 4 + 1];
 							c[4].b = rgba[(x + y * iw) * 4 + 0];
-							if(!alpha_0_is_unimportant<ColorDist>::value)
+							if(alpha_0_is_unimportant<ColorDist>::value)
 							{
 								if(dxt == DXT5)
 								{
